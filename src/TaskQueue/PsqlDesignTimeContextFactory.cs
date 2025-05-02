7ce69c8daab94c2b9a6c61dfd403af6544f2857b
Robert Zhang <louirobert@gmail.com>
2025-05-02 21:31:28 +0800

    TaskQueue: Read options for PsqlDesignTimeContextFactory from both ENV vars and CMD line.

--- src/TaskQueue/PsqlDesignTimeContextFactory.cs
+++ src/TaskQueue/PsqlDesignTimeContextFactory.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 using System.ComponentModel.DataAnnotations;
 
 namespace Rz.TaskQueue;
@@ -15,66 +16,28 @@ public class PsqlDesignTimeContextFactory : IDesignTimeDbContextFactory<PsqlCont
         public bool LogToConsole { get; set; }
     }
 
-    public static void ShowCommandLineHelp()
-    {
-        var cmd = Environment.GetCommandLineArgs()[0];
-        var name = Path.GetFileName(cmd);
-        var help = $"""
-Usage:
-    {name} <PsqlConnectionString> [-v PsqlVersion] [-l]
-
-Options:
-    -v: Specify database server version
-    -l: Log to console
-""";
-        Console.Error.WriteLine(help);
-    }
-
-    public static Options ProcessCommandLine(string[] args)
-    {
-        var options = new Options();
-        try
-        {
-            for (var i = 0; i < args.Length; i++)
-            {
-                switch (args[i])
-                {
-                    case "-h":
-                        ShowCommandLineHelp();
-                        Environment.Exit(0);
-                        break;
-                    case "-l":
-                        options.LogToConsole = true;
-                        break;
-                    case "-v":
-                        options.PgVersion = args[++i];
-                        break;
-                    default:
-                        if (options.PgConnectionString is null)
-                        {
-                            options.PgConnectionString = args[i];
-                        }
-                        else
-                        {
-                            throw new ArgumentException($"Unknown argument '{args[i]}'");
-                        }
-                        break;
-                }
-            }
-            Validator.ValidateObject(options, new ValidationContext(options));
-        }
-        catch (Exception ex)
-        {
-            Console.Error.WriteLine($"Error:\n{ex}");
-            ShowCommandLineHelp();
-            Environment.Exit(1);
-        }
-        return options;
-    }
-
     public PsqlContext CreateDbContext(string[] args)
     {
-        var options = ProcessCommandLine(args);
+        var switchMappings = new Dictionary<string, string>()
+        {
+            { "-c", "PgConnectionString" },
+            { "-v", "PgVersion" },
+            { "-l", "LogToConsole" },
+        };
+
+        var builder = new ConfigurationBuilder()
+            .AddEnvironmentVariables("RZ_")
+            .AddCommandLine(args, switchMappings);
+
+        var configuration = builder.Build();
+        var options = configuration.Get<Options>();
+
+        if (options == null)
+        {
+            throw new ArgumentException("PgConnectionString must be specified!");
+        }
+        Validator.ValidateObject(options, new ValidationContext(options));
+
         var factory = new PsqlContextFactory(options.PgConnectionString, options.PgVersion, (optBuilder) =>
         {
             if (options.LogToConsole)
