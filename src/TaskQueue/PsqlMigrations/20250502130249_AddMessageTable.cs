d493c17a214e2704778e6123e7ba1130da5b34bb
Robert Zhang <louirobert@gmail.com>
2025-05-02 21:33:28 +0800

    TaskQueue: Add PsqlMigrations/

---
+++ src/TaskQueue/PsqlMigrations/20250502130249_AddMessageTable.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Rz.TaskQueue.PsqlMigrations
+{
+    /// <inheritdoc />
+    public partial class AddMessageTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Receipt = table.Column<string>(type: "text", nullable: true),
+                    Queue = table.Column<string>(type: "text", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    RequeueCount = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    LeaseExpiredAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_CreatedAt",
+                table: "Messages",
+                column: "CreatedAt");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_LeaseExpiredAt",
+                table: "Messages",
+                column: "LeaseExpiredAt");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_Queue",
+                table: "Messages",
+                column: "Queue");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+        }
+    }
+}
